e4cf7a868e8f280e42744bf45fd6df9423dc959a
Pierre Tondereau <pierre.tondereau@gmail.com>
2016-06-06 20:33:16 +0200

    Update TalkServiceProvider and put files at the right place

--- src/TalkServiceProvider.php
+++ src/TalkServiceProvider.php
@@ -1,38 +1,85 @@
-<?php namespace Nahid\Talk;
+<?php
 
+namespace Nahid\Talk;
+
+use Illuminate\Container\Container;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
-use App;
+use Laravel\Lumen\Application as LumenApplication;
+use Nahid\Talk\Conversations\ConversationRepository;
+use Nahid\Talk\Messages\MessageRepository;
 
-class TalkServiceProvider extends ServiceProvider {
+class TalkServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->publishes([
-			 __DIR__.'/config' => base_path('config'),
-          __DIR__.'/migrations' => base_path('database/migrations')
-			]);
-	}
-
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        App::bind('Talk', function ($app) {
-      		return new Talk($app['Nahid\Talk\Conversations\ConversationRepository'], $app['Nahid\Talk\Messages\MessageRepository']);
-		});
-	}
-
-	public function provides()
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
     {
-        return ['Nahid\Talk\TalkServiceProvider'];
+        $this->setupConfig();
+        $this->setupMigrations();
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerTalk();
+    }
+
+    /**
+     * Setup the config.
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/../config/talk.php');
+        // Check if the application is a Laravel OR Lumen instance to properly merge the configuration file.
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('talk.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('talk');
+        }
+        $this->mergeConfigFrom($source, 'talk');
+    }
+
+    /**
+     * Publish migrations files.
+     */
+    protected function setupMigrations()
+    {
+        $this->publishes([
+            realpath(__DIR__ . '/../database/migrations/') => database_path('migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register Talk class.
+     *
+     * @return void
+     */
+    protected function registerTalk()
+    {
+        $this->app->singleton(Talk::class, function (Container $app) {
+            return new Talk($app[ConversationRepository::class], $app[MessageRepository::class]);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return string[]
+     */
+    public function provides()
+    {
+        return [
+            Talk::class,
+        ];
     }
 
 }
