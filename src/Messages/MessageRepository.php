9aa429284b5ab913e172635fb23677b547ba5e96
Iman <imanghafoori1@gmail.com>
2017-05-23 14:59:04 +0400

    Update MessageRepository.php
    just simpler code with the same logic

--- src/Messages/MessageRepository.php
+++ src/Messages/MessageRepository.php
@@ -28,20 +28,17 @@ class MessageRepository extends Repository
             $q->orWhere('user_two', $authUserId);
         }])->find($messageId);
 
-        if (!is_null($message->conversation)) {
-            if ($message->user_id == $authUserId) {
-                $message->deleted_from_sender = 1;
-            } else {
-                $message->deleted_from_receiver = 1;
-            }
-
-            $deleteMessage = $this->update($message);
-
-            if ($deleteMessage) {
-                return true;
-            }
+        if (is_null($message->conversation)) {
+            return false;
         }
 
-        return false;
+        if ($message->user_id == $authUserId) {
+            $message->deleted_from_sender = 1;
+        } else {
+            $message->deleted_from_receiver = 1;
+        }
+
+        return (boolean) $this->update($message);
+        
     }
 }
