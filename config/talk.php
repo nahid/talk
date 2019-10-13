bcfee52623eb2f01afb7ee35eba3cf72740eace9
Guklam <anonyguk@yahoo.fr>
2019-10-13 10:41:53 +0200

    Add .env support to the talk config file

--- config/talk.php
+++ config/talk.php
@@ -8,13 +8,13 @@ return [
     ],
     'broadcast' => [
         'enable' => false,
-        'app_name' => 'your-app-name',
+        'app_name' => env('PUSHER_APP_NAME'),
         'pusher' => [
-            'app_id' => '',
-            'app_key' => '',
-            'app_secret' => '',
+            'app_id' => env('PUSHER_APP_ID'),
+            'app_key' => env('PUSHER_APP_KEY'),
+            'app_secret' => env('PUSHER_APP_SECRET'),
             'options' => [
-                'cluster' => 'ap1',
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true
             ]
         ],
