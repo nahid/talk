527cb5b517e52c9a4913753208163c335249fc81
Damilola Olowookere <damms005@gmail.com>
2020-03-28 10:01:28 +0100

    fix src/Embera/Adapter.php according to https://github.com/nahid/talk/pull/135#discussion_r290658470

--- src/Embera/Adapter.php
+++ src/Embera/Adapter.php
@@ -8,10 +8,7 @@
 
 namespace Nahid\Talk\Embera;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> remotes/upstream/master
 use Embera\Adapters\Service;
 
 /**
@@ -21,39 +18,6 @@ use Embera\Adapters\Service;
  */
 class Adapter extends Service
 {
-<<<<<<< HEAD
-	protected $config;
-
-	/**
-	 * Adapter constructor.
-	 *
-	 * @param string $url
-	 * @param array  $config
-	 * @param        $oembed
-	 */
-	public function __construct($url, array $config = array(), $oembed)
-	{
-		$this->apiUrl = config('talk.oembed.url');
-		parent::__construct($url, $config, $oembed);
-	}
-
-	/**
-	 * Validates that the url belongs to this service.
-	 * Should be implemented on all children and should
-	 * return a boolean (preg_match returns 0 or 1 that
-	 * is why I'm also allowing 'int' as a return type).
-	 *
-	 * The current url is made available via $this->url
-	 *
-	 * @return bool|int
-	 */
-	protected function validateUrl()
-	{
-		$parsedUrl = parse_url($this->apiUrl);
-
-		return preg_match('#' . $parsedUrl['host'] . '#i', (string) $this->url);
-	}
-=======
     protected $config;
 
     /**
@@ -85,5 +49,4 @@ class Adapter extends Service
 
         return preg_match('#' . $parsedUrl['host'] . '#i', (string)$this->url);
     }
->>>>>>> remotes/upstream/master
 }
